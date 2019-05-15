f035d7dc9019f26a3daa6bbb8d32489f3603ed54
Matsca09 <1948700+Matsca09@users.noreply.github.com>
2019-05-15 16:27:08 +0200

    Update module controller
    - Update module status relying on nodes status
    - Add module requeue if:
      - its phase is "Running" or "Pending"
      - the module job is still running
    - Mark all node for deletion instead of deleting them if there are no replicas

--- pkg/controller/module/module_controller.go
+++ pkg/controller/module/module_controller.go
@@ -213,7 +213,7 @@ func (r *ReconcileModule) Reconcile(request reconcile.Request) (reconcile.Result
 		// Check if the job is still running
 		if found.Status.Active >= 1 {
 			glog.V(2).Infof("waiting for operate on job %s: it still has running pods\n", fmt.Sprintf("%s-job", instance.Name))
-			return reconcile.Result{}, fmt.Errorf("job %s is still running", fmt.Sprintf("%s-job", instance.Name))
+			return reconcile.Result{RequeueAfter: 20 * time.Second}, nil
 		}
 
 		// Cleanup Job pods
@@ -238,6 +238,16 @@ func (r *ReconcileModule) Reconcile(request reconcile.Request) (reconcile.Result
 		if err != nil {
 			return reconcile.Result{}, err
 		}
+
+		// Update status
+		err = r.updateModuleStatus(instance, corev1beta1.StatusPhasePending)
+		if err != nil {
+			glog.Errorf("cannot update status for module %s: %s\n", instance.Name, err.Error())
+			return reconcile.Result{}, err
+		}
+
+		// Requeue module update
+		return reconcile.Result{Requeue: true}, nil
 	}
 
 	appName := instance.ObjectMeta.Annotations["module.automium.io/appName"]
@@ -263,11 +273,12 @@ func (r *ReconcileModule) Reconcile(request reconcile.Request) (reconcile.Result
 	// Special cases (nonexistent, delete all)
 	if instance.Spec.Replicas == 0 && len(appNodes) > 0 {
 		// Delete all nodes
-		glog.V(2).Infof("service %s has no replicas - removing all nodes.\n", serviceName)
+		glog.V(2).Infof("service %s has no replicas - marking all nodes for deletion.\n", serviceName)
 		for _, item := range appNodes {
-			r.Delete(context.TODO(), &item)
+			item.Spec.DeletionDate = time.Now().String()
+			r.Update(context.TODO(), &item)
 		}
-		return reconcile.Result{}, nil
+		return reconcile.Result{Requeue: true}, nil
 	}
 
 	if len(appNodes) == 0 || len(appNodes) < instance.Spec.Replicas {
@@ -319,21 +330,6 @@ func (r *ReconcileModule) Reconcile(request reconcile.Request) (reconcile.Result
 			}
 		}
 
-		// Retrieve job status
-		status, err := r.retrieveJobStatus(request, found.Name, found.Namespace)
-		if err != nil {
-			glog.Errorf("cannot get status for job %s: %s\n", found.Name, err.Error())
-			return reconcile.Result{}, err
-		}
-
-		// Manage module status
-		glog.V(5).Infof("Updating module %s status...\n", instance.Name)
-		err = r.updateModuleStatus(request, status, instance.Spec.Replicas, 1, 1)
-		if err != nil {
-			glog.Errorf("cannot update module %s status: %s\n", instance.Name, err.Error())
-			return reconcile.Result{}, err
-		}
-		glog.V(5).Infof("Module %s updated\n", instance.Name)
 		return reconcile.Result{}, nil
 	}
 
@@ -376,6 +372,26 @@ func (r *ReconcileModule) Reconcile(request reconcile.Request) (reconcile.Result
 		}
 	}
 
+	// Retrieve job status
+	status, err := r.retrieveJobStatus(found)
+	if err != nil {
+		glog.Errorf("cannot get status for job %s: %s\n", found.Name, err.Error())
+		return reconcile.Result{}, err
+	}
+
+	// Manage module status
+	glog.V(5).Infof("Updating module %s status...\n", instance.Name)
+	err = r.updateModuleStatus(instance, status)
+	if err != nil {
+		glog.Errorf("cannot update module %s status: %s\n", instance.Name, err.Error())
+		return reconcile.Result{}, err
+	}
+	glog.V(5).Infof("Module %s updated\n", instance.Name)
+	if status == corev1beta1.StatusPhasePending || status == corev1beta1.StatusPhaseRunning {
+		glog.V(2).Infof("module %s is in Pending or Running -- reschedule update in 15s.\n", instance.Name)
+		return reconcile.Result{RequeueAfter: 15 * time.Second}, nil
+	}
+
 	glog.V(5).Infof("ops on module %s completed.\n", instance.Name)
 	return reconcile.Result{}, nil
 }
@@ -401,33 +417,25 @@ func sortNodesByNumber(nodes []corev1beta1.Node) error {
 	return globalErr
 }
 
-func (r *ReconcileModule) updateModuleStatus(request reconcile.Request, phase string, replicas, currentReplicas, updatedReplicas int) error {
-	currentModule := &corev1beta1.Module{}
-	err := r.Get(context.TODO(), request.NamespacedName, currentModule)
+func (r *ReconcileModule) updateModuleStatus(module *corev1beta1.Module, phase string) error {
+	module.Status.Phase = phase
+	module.Status.Replicas = module.Spec.Replicas
+
+	cur, upd, err := r.getUpdatedNodes(module.Namespace, module.ObjectMeta.Annotations["service.automium.io/name"], module.Spec.Action, module.Spec.Image)
 	if err != nil {
 		return err
 	}
+	module.Status.CurrentReplicas = cur
+	module.Status.UpdatedReplicas = upd
 
-	currentModule.Status.Phase = phase
-	currentModule.Status.Replicas = replicas
-	currentModule.Status.CurrentReplicas = currentReplicas
-	currentModule.Status.UpdatedReplicas = updatedReplicas
-
-	err = r.Status().Update(context.Background(), currentModule)
+	err = r.Status().Update(context.Background(), module)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (r *ReconcileModule) retrieveJobStatus(request reconcile.Request, jobName, jobNamespace string) (string, error) {
-	job := &batchv1.Job{}
-	err := r.Get(context.TODO(), types.NamespacedName{Name: jobName, Namespace: jobNamespace}, job)
-	if err != nil {
-		return "", err
-	}
-
-	glog.V(5).Infof("Job status: %+v\n", job.Status)
+func (r *ReconcileModule) retrieveJobStatus(job *batchv1.Job) (string, error) {
 
 	if job.Status.StartTime == nil {
 		return corev1beta1.StatusPhasePending, nil
@@ -448,5 +456,59 @@ func (r *ReconcileModule) retrieveJobStatus(request reconcile.Request, jobName, 
 		}
 	}
 
-	return "", goerrors.New(fmt.Sprintf("cannot detect job %s status", jobName))
+	return "", goerrors.New(fmt.Sprintf("cannot detect job %s status", job.Name))
+}
+
+func (r *ReconcileModule) getUpdatedNodes(namespace, serviceName, action, newImageVersion string) (int, int, error) {
+	var currentReplicas int
+	var updatedReplicas int
+
+	if namespace == "" && serviceName == "" && newImageVersion == "" {
+		return -1, -1, goerrors.New("invalid parameters for retrieving application data from nodes")
+	}
+
+	nodesList := &corev1beta1.NodeList{}
+	err := r.List(context.TODO(), &client.ListOptions{Namespace: namespace}, nodesList)
+	if err != nil {
+		return -1, -1, err
+	}
+
+	for _, node := range nodesList.Items {
+		if node.ObjectMeta.Annotations["service.automium.io/name"] == serviceName {
+			if nodeIsHealthy(node) {
+				if node.Status.NodeProperties.Image == newImageVersion {
+					glog.V(5).Infof("getUpdatedNodes: node: %s - is an updated replica\n", node.Name)
+					updatedReplicas++
+					if action == "Deploy" {
+						currentReplicas++
+					}
+				} else {
+					glog.V(5).Infof("getUpdatedNodes: node: %s - is a current replica\n", node.Name)
+					currentReplicas++
+				}
+			} else {
+				glog.V(5).Infof("getUpdatedNodes: node: %s is not healty - skipping\n", node.Name)
+			}
+		}
+	}
+
+	return currentReplicas, updatedReplicas, nil
+
+}
+
+func nodeIsHealthy(node corev1beta1.Node) bool {
+
+	// Check the node ID
+	if node.Status.NodeProperties.ID == "non-existent-machine" {
+		return false
+	}
+
+	// Check if registered services are not in "passing" status
+	for _, svc := range node.Status.NodeHealthChecks {
+		if svc.Status != "passing" {
+			return false
+		}
+	}
+
+	return true
 }
