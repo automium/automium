7235d0679a2c910c38c89871631ac2711517479b
Matsca09 <1948700+Matsca09@users.noreply.github.com>
2019-05-27 14:39:54 +0200

    Add service name to node

--- pkg/apis/core/v1beta1/node_types.go
+++ pkg/apis/core/v1beta1/node_types.go
@@ -61,10 +61,10 @@ type NodeStatus struct {
 // +k8s:openapi-gen=true
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Hostname",type="string",JSONPath=".spec.hostname",description="the node hostname"
-// +kubebuilder:printcolumn:name="Consul-ID",type="string",JSONPath=".status.nodeProperties.id",description="the Consul node ID"
 // +kubebuilder:printcolumn:name="Internal-IP",type="string",JSONPath=".status.nodeProperties.address",description="the node private IP"
 // +kubebuilder:printcolumn:name="External-IP",type="string",JSONPath=".status.nodeProperties.publicAddress",description="the node public IP"
 // +kubebuilder:printcolumn:name="OS-Image",type="string",JSONPath=".status.nodeProperties.image",description="the image deployed on node"
+// +kubebuilder:printcolumn:name="Service",type="string",JSONPath=".metadata.annotations['service\.automium\.io/name']",description="Service which the node belongs to"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 type Node struct {
 	metav1.TypeMeta   `json:",inline"`
